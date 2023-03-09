4a4574de0f65b3a675ac7b1e2397e073ff22bb0b
Jonasqwerty <106714952+Jonasqwerty@users.noreply.github.com>
2023-03-09 17:22:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-/**
-cors - Compartilhamento de Recursos de Origem Cruzada
-    
-    - corrige os problemas de requisições sendo enviadas
-        para o mesmo domínio. Quando estamos rodando a API
-        e o front-end, o navegador poderá bloquear. 
-        O Cors realiza esse desvio a proteção que existe 
-        no próprio navegador
- */
+
 app.use(express.json())
-//app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))// porta 3000 front end
-app.use(cors({ credentials: true, origin: 'http://localhost:8080' }))// porta 3000 front end - Vuejs
-//app.use(express.static('public'))
-//rotas
+
+app.use(cors({ credentials: true, origin: 'http://localhost:8080' }))
+
 const AdminRoutes = require('./routes/AdminRoutes')
 app.use('/', AdminRoutes)
-//servidor
+
 const AlunoRoutes = require('./routes/AlunoRoutes')
 app.use('/aluno', AlunoRoutes)
 
